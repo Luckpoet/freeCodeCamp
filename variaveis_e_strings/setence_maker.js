17165c2a75e7f1bcfa442bf0dd3937d5dc186319
Lucas Oliveira <lukas.ass0206@gmail.com>
2026-01-21 20:36:45 -0300

    Arquivos organizados

---
+++ variaveis_e_strings/setence_maker.js
@@ -0,0 +1,58 @@
+// Exercício do FreeCodeCamp adptado para ficar mais interessante =)
+
+let adjective = 'sábio homem chamado';
+let noun = 'Dante';
+let verb = 'ardendo em amor e fé';
+let place = 'exílio distante do convívio humano';
+let adjective2 = 'eternamente';
+let noun2 = 'Beatriz e o caminho entre Inferno e Céu';
+
+let firstStory = `
+No meio da senda turva da existência
+ergue-se ${adjective} ${noun}, guiado por desvelo,
+buscando no alto a chama da transcendência.
+
+No ${place} aprende o peso do zelo,
+pois traz ${adjective2} os olhos em Beatriz,
+e nela encontra dor que o purifica em anelo.
+`;
+
+console.log(`Primeira história:\n${firstStory}`);
+
+adjective = 'tolo homem chamado';
+noun = 'Jones';
+verb = 'estava bêbado e esquecia de alimentar os animais';
+place = 'Granja Solar';
+adjective2 = 'animais atentos, cheios de ódio contido';
+noun2 = 'carne em excesso enquanto explorava seus animais';
+
+let secondStory = `
+Era uma vez um ${adjective} ${noun},
+que se deleitava com ${noun2}.
+${noun} vivia na ${place},
+onde ${adjective2} aguardavam o momento certo,
+sempre que ele ${verb}.
+`;
+
+console.log(`Segunda história:\n${secondStory}`);
+/*
+Caso você deseje ver a primeira versão, aqui esta ela:
+let adjective = 'Sábio homem chamado';
+let noun = 'Dante';
+let verb = "Doente de Amor";
+let place = "Ilha, isolado da sociedade";
+let adjective2 = "Voltadas apenas aos olhos de Beatriz"; 
+let noun2 = 'Figos e fazer caminhadas no Inferno rumo ao Céu';
+
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`; 
+console.log(First story: ${firstStory}) 
+
+adjective = 'Tolo homem chamado'; 
+noun = 'Jones'; 
+verb = "Estava bebado e esquecia de dar comida para eles"; 
+place = "Granja Solar"; 
+adjective2 = "Animais malignos, prontos para fazer uma revolução"; 
+noun2 = 'Carne de porco e maltratar seus animais'; 
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`; 
+console.log(Second story: ${secondStory});
+*/
