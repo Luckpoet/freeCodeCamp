f1e52e398290fe106f1e5261f47314a06d1ed481
Lucas Oliveira <lukas.ass0206@gmail.com>
2026-01-21 20:06:35 -0300

    Arquivos organizados

---
+++ variaveis_e_strings/trivia_bot.js
@@ -0,0 +1,20 @@
+// Nota: o texto está bilingue porque o FreeCodeCamp exige que as mensagens do console estejam em inglês.
+console.log("Hello! I'm your coding fun fact guide!");
+var botName = 'Carlos Ycaro';
+var botLocation = 'Fortaleza - Ceará';
+var favoriteLanguage = 'Brainfuck';
+
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+
+let codingFact = `Você sabia que a tradução de ${favoriteLanguage} é "Foda-se o cérebro?"`;
+console.log(codingFact);
+
+codingFact = `${favoriteLanguage} tem apenas 8 comandos: (>; <; +; -; .; ,; [; ]), cada um representando uma ação direta na memória.`
+console.log(codingFact);
+
+codingFact = `Você não deveria gastar seu tempo tentando aprender ${favoriteLanguage}`;
+console.log(codingFact);
+
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`)
+
